cbfe92eb959aa9f8d99bc68b2d6d9830c8bc3f17
Cecilie Dura André <CecilieAndre@Cecilies-MacBook-Pro-6.local>
2020-03-11 17:22:50 -0400

    pylinked

--- Hyperparameter Optimization/test_Project_CSME802.py
+++ Hyperparameter Optimization/test_Project_CSME802.py
@@ -1,60 +1,64 @@
+""" This code is used to test the script: project_csme802_2_2.py. """
+
 #!/usr/bin/env python
 # coding: utf-8
 
-import Project_CSME802_2
-import numpy as np
-import torch
 import unittest
+import numpy as np
+import project_csme802
 
-class best_arm1:
-        parameters = {}
-        parameters["lr"] = 0.001
-        parameters["momentum"] = 0.001
-        parameters["weight_decay"] = 0.001
+
+class Parameters:
+    """ Parameters used to test the code """
+    parameters = {}
+    parameters["lr"] = 0.001
+    parameters["momentum"] = 0.001
+    parameters["weight_decay"] = 0.001
+
 
 class TestCode(unittest.TestCase):
-
+    """ Testing the code """
     def test_load_data(self):
-        
-        data = Project_CSME802_2.load_data()
-        
-        self.assertEqual( data['X_train'].shape , (50000, 1, 60, 60) )
-        self.assertEqual( data['X_valid'].shape , (10000, 1, 60, 60) )
-        self.assertEqual( data['X_test'].shape , (10000, 1, 60, 60) )
+        """ Testing the load_data-function """
+        data = project_csme802.load_data()
 
-    
-    
-    def test_eval_BO(self):
-        
-        net  = Project_CSME802_2.Net()
-        data = Project_CSME802_2.load_data()
-        accuracy = Project_CSME802_2.eval_BO(net=net, Input=data['X_valid'], Label=data['y_valid'],)
-        
-        self.assertEqual( type(accuracy), np.float64 )
-        self.assertEqual( accuracy.shape, () )
+        self.assertEqual(data['x_train'].shape, (50000, 1, 60, 60))
+        self.assertEqual(data['x_valid'].shape, (10000, 1, 60, 60))
+        self.assertEqual(data['x_test'].shape, (10000, 1, 60, 60))
 
-    
+    def test_eval_bayesian_optimization(self):
+        """ Testing the eval_bayesian_optimization-function """
+        net = project_csme802.Net()
+        data = project_csme802.load_data()
+        accuracy = project_csme802.eval_bayesian_optimization(
+            net=net, input_picture=data['x_valid'], label_picture=data['y_valid'],)
+
+        self.assertEqual(type(accuracy), np.float64)
+        self.assertEqual(accuracy.shape, ())
+
     def test_train_epoch(self):
-        
-        net  = Project_CSME802_2.Net()
-        data = Project_CSME802_2.load_data()
-        best_arm = best_arm1()
-        cost_mean, accuracy = Project_CSME802_2.train_epoch(net=net, Input=data['X_train'],Label=data['y_train'], parameters=best_arm.parameters,)
-        
-        self.assertEqual( type(accuracy), np.float64 )
-        self.assertEqual( type(cost_mean), np.float32 )
-        self.assertEqual( accuracy.shape, () )
-        self.assertEqual( cost_mean.shape, () )
+        """ Testing the train_epoch-function """
+        net = project_csme802.Net()
+        data = project_csme802.load_data()
+        best_arm = Parameters()
+        cost_mean, accuracy = project_csme802.train_epoch(net=net,\
+            input_picture=data['X_train'], label_picture=data['y_train'],\
+            parameters=best_arm.parameters,)
 
+        self.assertEqual(type(accuracy), np.float64)
+        self.assertEqual(type(cost_mean), np.float32)
+        self.assertEqual(accuracy.shape, ())
+        self.assertEqual(cost_mean.shape, ())
 
     def test_eval_epoch(self):
-        
-        net  = Project_CSME802_2.Net()
-        data = Project_CSME802_2.load_data()
-        accuracy = Project_CSME802_2.eval_epoch( net=net, Input=data['X_valid'], Label=data['y_valid'],)
-        
-        self.assertEqual( type(accuracy), np.float64 )
-        self.assertEqual( accuracy.shape, () )
+        """ Testing the eval_epoch-function """
+        net = project_csme802.Net()
+        data = project_csme802.load_data()
+        accuracy = project_csme802.eval_epoch(
+            net=net, input_picture=data['X_valid'], label_picture=data['y_valid'],)
+
+        self.assertEqual(type(accuracy), np.float64)
+        self.assertEqual(accuracy.shape, ())
 
 
 if __name__ == "__main__":
